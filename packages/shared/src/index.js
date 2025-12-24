049667164f9285b80dc14f3c7f979c56d03806fb
yunmu <1416900346@qq.com>
2025-12-25 01:57:51 +0800

    refactor(shared): 使用 ES 模块语法重构导出代码
    - 在 shared 包中的 package.json 添加 type 为 module，启用 ES 模块支持
    - 移除 backend tsconfig.json 中的 rootDir 配置
    - 将 shared 包 src/index.js 文件由 CommonJS 语法改为原生 ES 模块导出语法
    - 清理不再需要的样板导出代码
    - 保持 sourcemap 文件同步更新，确保映射正确性

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -1,23 +1,7 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
 // Zod Schemas (Single Source of Truth)
-__exportStar(require("./schemas/auth.schema"), exports);
+export * from './schemas/auth.schema';
 // 通用响应类型
-__exportStar(require("./dto/common.dto"), exports);
+export * from './dto/common.dto';
 // 工具函数
-__exportStar(require("./utils/user.utils"), exports);
+export * from './utils/user.utils';
 //# sourceMappingURL=index.js.map
